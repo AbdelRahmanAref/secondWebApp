11809bb6b1b2c635c6630fc480b8adc2ad0028f0
AbdelRahman Aref <abdelrahman_aref@hotmail.com>
2019-04-26 12:05:44 +0200

    firstwebapp

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,19 +2,17 @@ import Vapor
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
-    // Basic "It works" example
-    router.get { req in
-        return "It works!"
-    }
-    
-    // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
-    }
 
-    // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
+    router.get("name"){ request in
+        return "AbdelRahman Aref"
+    }
+    router.get("json"){ request in
+        return Family(papa: "AbdelRahman", mama: "Mai", kid: "Haroun")
+    }
+}
+struct Family: Content{
+    var papa: String
+    var mama: String
+    var kid: String
+
 }
